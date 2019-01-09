5cca0f75a876b256ba636e15c75ae8fb3cbc7fc5
ANIL <ANIL@ANIL-PC>
2019-01-09 23:08:16 +0530

    initial commit

---
+++ src/main/java/com/holidays/BusinessHolidaysApplication.java
@@ -0,0 +1,14 @@
+package com.holidays;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessHolidaysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessHolidaysApplication.class, args);
+	}
+
+}
+
