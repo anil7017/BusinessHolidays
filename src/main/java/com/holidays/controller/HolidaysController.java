37236eaac7a4fc42b9ca90d728d4f32cb9c97530
ANIL <ANIL@ANIL-PC>
2019-01-19 23:52:05 +0530

    add comment messgae

--- src/main/java/com/holidays/controller/HolidaysController.java
+++ src/main/java/com/holidays/controller/HolidaysController.java
@@ -2,4 +2,5 @@ package com.holidays.controller;
 
 public class HolidaysController {
 
+	
 }
--- src/main/java/com/holidays/model/JavaDateUtil.java
+++ src/main/java/com/holidays/model/JavaDateUtil.java
@@ -43,6 +43,7 @@ public class JavaDateUtil {
 		System.out.println(date.getDayOfWeek());
 		
 		
+		
 		return null;
 	}
 }
